b8a4ec7f33d0d158b55d758ae4d14d80a94de29f
Editmkal <90159943+Editmkal@users.noreply.github.com>
2021-09-06 10:33:41 +0200

    Add files via upload

---
+++ tpEditMutsoKalamire/include/typecom.h
@@ -0,0 +1,16 @@
+#ifndef TYPECOM_H
+#define TYPECOM_H
+
+
+class typecom
+{
+    public:
+        typecom();
+        virtual ~typecom();
+
+    protected:
+
+    private:
+};
+
+#endif // TYPECOM_H
